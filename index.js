844abdb69d16888bbc449219084eb1d190d65723
Jon Bierma <jon.bierma@gmail.com>
2017-09-15 23:36:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  const target = document.getElementById('nested').getElementsByClassName('target')
+  return target[0]
+}
+
+function increaseRankBy(n){
+  const incrementer = document.querySelectorAll('ul.ranked-list li')
+  const nParsed = parseInt(n)
+  for (let i = 0; i <incrementer.length; i++){
+    var j=parseInt(incrementer[i].innerHTML)
+    incrementer[i].innerHTML = (j+nParsed).toString()
+  }
+}
+
+function deepestChild(){
+  var nash = document.getElementById('grand-node')
+  while (nash.children[0]){
+    nash= nash.children[0]
+  }
+  return nash
+}
