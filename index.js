07cdcf3aad189dab604bcf9609ddc180dc59a651
Jon Bierma <jon.bierma@gmail.com>
2017-09-15 23:20:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  const target = document.getElementsByIdName('nested').getelementByClassNameI('target')
+  return target
+}
+
+function increaseRankBy(n){
+  const incrementer = document.getElementByTag('li').querySelectorAll('ul.ranked-list li')
+  const nParsed = parseInt(n)
+  for (let i = 0; i <incrementer.length; i++){
+    incrementer[i].innerHTML = (i+nParsed).toString()
+  }
+}
+
+function deepestChild(){
+  const nash = document.getElementsByIdName('grand-node').querySelector('div')
+  while (nash.children()){
+    nash= nash.children()
+  }
+  return nash
+}
